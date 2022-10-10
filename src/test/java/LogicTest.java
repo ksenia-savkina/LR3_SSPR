74749b4230491ecf3e9d297f3ac84131e3a72e81
Ksenia <ksenia.savkina2001@gmail.com>
2022-10-10 20:38:12 +0400

    Неправильное значение в функции тестирования

--- src/test/java/LogicTest.java
+++ src/test/java/LogicTest.java
@@ -8,7 +8,7 @@ public class LogicTest {
     @Test
     public void checkPasswordTest() {
         Logic logic = new Logic();
-        var result = logic.checkPassword("qq!!d");
+        var result = logic.checkPassword("qq!!");
         Assertions.assertTrue(result);
     }
 
